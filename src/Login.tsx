0feac9b139263d59a32d57a8c1a751ad8f9081d2
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-05-20 11:09:00 +0900

    buttonにidを追加

--- src/Login.tsx
+++ src/Login.tsx
@@ -125,6 +125,7 @@ const Login = () => {
               )}
             </div>
             <button
+              id="loginbutton"
               onClick={onMailLogin}
               type="button"
               className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
