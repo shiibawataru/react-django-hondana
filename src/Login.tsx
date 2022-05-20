0193cb744e33122221bf74725c05c5366dbefa90
shiibawataru <wataru.shiiba@rakus-partners.co.jp>
2022-05-20 18:00:08 +0900

    placeholderの修正

--- src/Login.tsx
+++ src/Login.tsx
@@ -112,7 +112,7 @@ const Login = () => {
                 type="password"
                 name="password"
                 id="password"
-                placeholder="••••••••"
+                placeholder="••••••"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                 required
               />
